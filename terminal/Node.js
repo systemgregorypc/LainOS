a0adf40ad313161674649c293548a8a14c9f9a0b
jose gregorio hernandez calderon <systemgregoryp.c@gmail.com>
2020-07-23 21:21:35 -0400

    Create Node.js

---
+++ terminal/Node.js
@@ -0,0 +1,22 @@
+# curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
+OR
+# wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
+
+# command -v nvm
+# nvm ls-remote
+# nvm install 10.15.3  	#or 8.16.0, 11.15.0 etc
+# nvm ls
+# nvm use node	#use default
+OR
+# nvm use 10.15.3
+# nvm use node	#use default
+OR
+# nvm use 10.15.3
+# nvm which 10.15.3
+# nvm which 12.2.0
+# nvm which system  #check system-installed version of a node using “system” alias
+# nvm alias default 10.15.3
+# nvm alias default system
+# nvm alias default 12.2.0
+nvm --help
+ https://github.com/nvm-sh/nvm.
